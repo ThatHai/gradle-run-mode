f1693b1276dd9b7ff43a3f8719174bdc4eea742f
Hai <62766020+ThatHai@users.noreply.github.com>
2020-06-01 20:26:23 -0700

    Rename mode to gradle-run-mode
    and fix key-bindig bug.

--- gradle-run-mode.el
+++ gradle-run-mode.el
@@ -5,78 +5,71 @@
 ;;; Code:
 (require 'compile)
 
-(defcustom gradle-mode-read-build-args t
-  "Non nil to prompt for build arguments by gradle-run."
-  :group 'gradle
-  :type 'boolean)
-
-(defcustom gradle-mode-build-args nil
+(defcustom gradle-run-build-args nil
   "Build arguments for gradle."
   :group 'gradle
   :type 'string)
 
-(defcustom gradle-mode-env-vars nil
+(defcustom gradle-run-env-vars nil
   "Build arguments for gradle."
   :group 'gradle
   :type 'string)
 
-(defvar gradle-mode-gradlew-command (if (string= system-type "windows-nt") "gradlew.bat" "gradlew")
+(defvar gradle-run-gradlew-command (if (string= system-type "windows-nt") "gradlew.bat" "gradlew")
   "The gradle wrapper command.")
 
-(defun gradle-mode--get-root ()
+(defun gradle-run--get-root ()
   "Return the root directory of the gradle project."
   (let ((root-dir (or (and (featurep 'projectile) (projectile-project-p))
-                      (locate-dominating-file default-directory gradle-mode-gradlew-command))))
+                      (locate-dominating-file default-directory gradle-run-gradlew-command))))
     (or root-dir
         (user-error "No gradle project to run"))))
 
-(defun gradle-mode-set-env ()
-  "Promt to read the environment variables setting for gradle-mode-env-vars."
+(defun gradle-run-set-env ()
+  "Promt to read the environment variables setting for gradle-run-env-vars."
   (interactive)
-  (setq gradle-mode-env-vars
-        (read-from-minibuffer "Env variables: " gradle-mode-env-vars)))
+  (setq gradle-run-env-vars
+        (read-from-minibuffer "Env variables: " gradle-run-env-vars)))
 
-(defun gradle-mode-run ()
-  "Run gradle command by interactively read arguments to run."
+(defun gradle-run ()
+  "Run gradlew command by interactively read arguments to run."
   (interactive)
-  (when gradle-mode-read-build-args
-      (setq gradle-mode-build-args
-            (read-from-minibuffer "Build args: " gradle-mode-build-args)))
-  (gradle-mode--run gradle-mode-build-args gradle-mode-env-vars))
+  (setq gradle-run-build-args
+        (read-from-minibuffer "Build args: " gradle-run-build-args))
+  (gradle-run--exec gradle-run-build-args gradle-run-env-vars))
 
-(defun gradle-mode-build ()
+(defun gradle-run-build ()
   "Run gradle build task."
   (interactive)
-  (gradle-mode--run "build" gradle-mode-env-vars))
+  (gradle-run--exec "build" gradle-run-env-vars))
 
-(defun gradle-mode--run (build-args env-vars)
+(defun gradle-run--exec (build-args env-vars)
   "Run gradle with the specified build arguments and environment variables.
 
 BUILD-ARGS the gradle build arguments to run.
 ENV-VARS the environment variables to use when running gradle."
 
-  (let* ((run-directory (gradle-mode--get-root))
+  (let* ((run-directory (gradle-run--get-root))
          (saved-default-directory default-directory)
-         (gradlew-cmd (f-join run-directory gradle-mode-gradlew-command)))
+         (gradlew-cmd (f-join run-directory gradle-run-gradlew-command)))
     (cd run-directory)
     (compile (concat env-vars " " gradlew-cmd " " build-args))
     (cd saved-default-directory)))
 
-(defvar gradle-mode-map
+(defvar gradle-run-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-c C-g b") 'gradle-mode-build)
-    (define-key map (kbd "C-c C-g r") 'gradle-mode-run)
+    (define-key map (kbd "C-c C-g b") 'gradle-run-build)
+    (define-key map (kbd "C-c C-g r") 'gradle-run)
     map)
   "Keymap for the gradle minor mode.")
 
 ;;;###autoload
-(define-minor-mode gradle-mode
+(define-minor-mode gradle-run-mode
   "Emacs minor mode for integrating Gradle into compile.
-Run gradle tasks from any buffer, scanning up to nearest gradlew
-directory to run tasks."
+Run gradle tasks from any buffer, scanning up to the gradle project root directory to run tasks."
   :lighter " Gradle"
-  :keymap 'gradle-mode-map
+  :keymap 'gradle-run-mode-map
   :global t)
 
-(provide 'gradle-mode)
-;;; gradle-mode.el ends here
+(provide 'gradle-run-mode)
+;;; gradle-run-mode.el ends here
